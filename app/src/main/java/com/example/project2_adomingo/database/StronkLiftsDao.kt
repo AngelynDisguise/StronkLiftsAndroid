491528bf7667b9adacf72ae5b4aa770fc69b44d9
AngelynDisguise <angelyn.domingo122@gmail.com>
2024-04-08 14:34:45 -0400

    Removed workout dates, added plan schedule. Updated DAO interface.

--- app/src/main/java/com/example/project2_adomingo/database/StronkLiftsDao.kt
+++ app/src/main/java/com/example/project2_adomingo/database/StronkLiftsDao.kt
@@ -14,19 +14,38 @@ import java.time.LocalDate
 interface StronkLiftsDao {
 
     /* * * * * *  CREATE * * * * * */
+    // IMPORTANT NOTE: only one user (userId = 0) should exist for now
+    // Change queries and methods later to have userId parameter if multiple users are an option
 
-    // Insert a workout plan along with all the associated entities
+    // USER
+
+    // Add user
+    // Note: only one user should exist for now, inserting will overwrite
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertUser(user: User)
+
+    // WORKOUT PLAN
+
+    // Add a workout, overwriting if the workout already exists
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertWorkout(workout: Workout)
+
+
+    // Insert an exercise for a workout, overwriting if the exercise already exists in the workout
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertWorkoutExercise(workoutExercise: WorkoutExercise)
+
+    // Insert a workout plan, and the embedded workout
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertWorkoutPlan(workoutPlan: WorkoutPlan)
+
+    // Add a workout plan along with all the associated entities
     @Transaction
     fun insertWorkoutPlanWithDetails(workoutPlan: WorkoutPlan) {
         val workoutId = workoutPlan.workout.workoutId
 
         // Insert Workout
         insertWorkout(workoutPlan.workout)
-
-        // Insert Date (workoutId should exist)
-        for (date in workoutPlan.dates) {
-            insertDate(Date(date.dateId, workoutId, date.date))
-        }
 
         // Insert WorkoutExercise (workoutId and exerciseId should exist)
         for (exercise in workoutPlan.exercises) {
@@ -37,52 +56,45 @@ interface StronkLiftsDao {
         insertWorkoutPlan(workoutPlan.copy(workout = workoutPlan.workout.copy(workoutId = workoutId)))
     }
 
-    // Add a workout, overwriting if the workout already exists
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertWorkout(workout: Workout)
-
-    // Add an exercise, only if the exercise does not already exist
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    // EXERCISE
+    @Insert(onConflict = OnConflictStrategy.IGNORE) // prevent overwrite
     fun insertExercise(exercise: Exercise)
 
-    // Insert a date for a workout, overwriting if the date already exists in the workout
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertDate(date: Date)
+    // SCHEDULE
 
-    // Insert an exercise for a workout, overwriting if the exercise already exists in the workout
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertWorkoutExercise(workoutExercise: WorkoutExercise)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertScheduleDate(scheduleDate: ScheduleDate)
 
-    // Insert a workout plan, and the embedded workout
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertWorkoutPlan(workoutPlan: WorkoutPlan)
+    @Transaction
+    fun insertSchedule(scheduleDates: List<ScheduleDate>) {
+        scheduleDates.forEach() { scheduleDate -> insertScheduleDate(scheduleDate) }
+    }
 
 
     /* * * * * *  READ  * * * * * */
+    // IMPORTANT NOTE: only one user (userId = 0) should exist for now
 
-    // Get all workout plans
+    // USER INFO
+
+    // Get a specific user
+    @Query("SELECT * FROM users WHERE userId = :userId")
+    fun getUser(userId: Long): User
+
+    // WORKOUT PLAN
+
+    // Get all workouts in plan (only one plan exists for now)
     @Transaction
-    @Query("SELECT * FROM workouts")
-    fun getAllWorkoutPlans(): LiveData<List<WorkoutPlan>>
+    @Query("SELECT * FROM workouts WHERE userId = :userId")
+    fun getAllWorkoutPlans(userId: Long): LiveData<List<WorkoutPlan>>
 
-    // Get all exercises
-    @Query("SELECT * FROM exercises")
-    fun getAllExercises(): LiveData<List<Exercise>>
+    // Get a specific workout in a plan (has the exercises and dates)
+    @Query("SELECT * FROM workouts WHERE workoutId = :workoutId AND userId = :userId")
+    fun getWorkoutPlan(userId: Long, workoutId: Long): WorkoutPlan
 
-    // Get a specific workout (has the exercises and dates)
-    @Query("SELECT * FROM workouts WHERE workoutId = :workoutId")
-    fun getWorkoutPlan(workoutId: Long): WorkoutPlan
-
-    // Get all workout exercises from a workout
+    // Get all workout exercises in a workout plan
     @Transaction
-    fun getAllWorkoutExercises(workoutId: Long): List<WorkoutExercise>? {
-        return getWorkoutPlan(workoutId).exercises
-    }
-
-    // Get all dates from a workout
-    @Transaction
-    fun getAllWorkoutDates(workoutId: Long): List<Date>? {
-        return getWorkoutPlan(workoutId).dates
+    fun getAllWorkoutExercises(userId: Long, workoutId: Long): List<WorkoutExercise>? {
+        return getWorkoutPlan(userId, workoutId).exercises
     }
 
     // Get all workout exercise names from a workout plan
@@ -90,30 +102,54 @@ interface StronkLiftsDao {
         return workoutPlan.exercises.map { it.exercise.exerciseName }
     }
 
-    // Get all workout exercise names from a workout plan by ID
-//    fun getAllWorkoutExerciseNamesById(workoutId: Long): List<String> {
-//        val exercises: List<WorkoutExercise>? = getAllWorkoutExercises(workoutId)
-//        return exercises.map { it.exerciseName }
-//    }
+    // WORKOUT HISTORY
 
-    // Get current workout session (assuming there is only one workout session in a day)
+    // Get workout history (has workout date and exercises)
     @Transaction
-    @Query("SELECT * FROM workout_history WHERE date = :today")
-    fun getCurrentWorkoutSession(today: LocalDate = LocalDate.now()): LiveData<List<ExerciseHistory>>
+    @Query("SELECT * FROM workout_history_dates WHERE userId = :userId")
+    fun getWorkoutHistory(userId: Long): LiveData<List<WorkoutHistory>>
+
+    // Get a workout from workout history
+    // Assumption: User does one workout a day (each workout has only one date)
+    // Note: Useful for getting today's workout session
+    @Transaction
+    @Query("SELECT * FROM workout_history_dates WHERE date = :date AND userId = :userId")
+    fun getWorkoutFromHistory(userId: Long, date: LocalDate): WorkoutHistory
+
+    @Transaction
+    @Query("SELECT * FROM workout_history_dates WHERE date = :today AND userId = :userId")
+    fun getTodaysWorkout(userId: Long, today: LocalDate = LocalDate.now()): WorkoutHistory
+
+    // EXERCISES
+
+    // Get all available exercises
+    @Query("SELECT * FROM exercises WHERE userId = 0")
+    fun getAllExercises(): LiveData<List<Exercise>>
+
+    // Get number of all available exercises
+    @Query("SELECT COUNT(*) FROM exercises WHERE userId = 0")
+    fun getExerciseCount(): Int
+
+    // SCHEDULE
+
+    @Query("SELECT * FROM workouts WHERE userId = 0")
+    fun getUserSchedule() :LiveData<List<ScheduleDate>>
 
     /* * * * * *  UPDATE  * * * * * */
+    // IMPORTANT NOTE: only one user (userId = 0) should exist for now
+    // Change queries and methods later to have userId parameter if multiple users are an option
+
+    // USER INFO
+
+    // Update a user
+    @Update
+    fun updateWorkout(user: User)
+
+    // WORKOUT PLAN
 
     // Update a workout
     @Update
     fun updateWorkout(workout: Workout)
-
-    // Update an exercise
-    @Update
-    fun updateExercise(exercise: Exercise)
-
-    // Update a date
-    @Update
-    fun updateDate(date: Date)
 
     // Insert or update a workout plan with details
     @Transaction
@@ -126,17 +162,26 @@ interface StronkLiftsDao {
         // Insert the updated WorkoutPlan
         insertWorkoutPlan(workoutPlan)
 
-        // Insert Date
-        for (date in workoutPlan.dates) {
-            insertDate(date)
-        }
-
         // Insert WorkoutExercise
         for (exercise in workoutPlan.exercises) {
             insertWorkoutExercise(exercise)
         }
     }
 
+    // EXERCISE
+
+    // Update an exercise
+    @Update
+    fun updateExercise(exercise: Exercise)
+
+    // SCHEDULE
+
+    // Should just add and delete instead
+    @Transaction
+    fun updateSchedule(scheduleDates: List<ScheduleDate>) {
+        deleteAllScheduleDates()
+        scheduleDates.forEach{ scheduleDate -> insertScheduleDate(scheduleDate) }
+    }
 
     /* * * * * *  DELETE  * * * * * */
 
@@ -145,35 +190,46 @@ interface StronkLiftsDao {
     fun deleteExerciseById(exerciseId: Long)
     // need to also refresh workout plans, maybe call getAllWorkoutPlans() again?
 
-
     // Delete a workout (only called in deleteWorkoutPlan)
     @Delete
     fun deleteWorkout(workoutId: Long)
-
 
     // Delete all workout exercises by workoutId (only called in deleteWorkoutPlan)
     @Query("DELETE FROM workout_exercises WHERE workoutId = :workoutId")
     fun deleteAllWorkoutExercises(workoutId: Long)
 
-    // Delete all dates by workoutId (only called in deleteWorkoutPlan)
-    @Query("DELETE FROM dates WHERE workoutId = :workoutId")
-    fun deleteAllWorkoutDates(workoutId: Long)
-
-//    // Delete a specific workout exercise by workoutId and exerciseId
-//    @Query("DELETE FROM workout_exercises WHERE workoutId = :workoutId AND exerciseId = :exerciseId")
-//    fun deleteWorkoutExercise(workoutId: Long, exerciseId: Long)
-//
-//    // Delete a specific date by workoutId and dateId
-//    @Query("DELETE FROM dates WHERE workoutId = :workoutId AND dateId = :dateId")
-//    fun deleteWorkoutDate(workoutId: Long, dateId: Long)
+    // Delete a specific workout exercise by workoutId and exerciseId
+    @Query("DELETE FROM workout_exercises WHERE userId = :userId AND workoutId = :workoutId AND exerciseId = :exerciseId")
+    fun deleteWorkoutExercise(userId: Long, workoutId: Long, exerciseId: Long)
 
     // Delete a workout plan
     @Transaction
     fun deleteWorkoutPlan(workoutId: Long) {
         deleteAllWorkoutExercises(workoutId)
-        deleteAllWorkoutDates(workoutId)
         deleteWorkout(workoutId)
     }
+
+    // EXERCISE
+
+    // Not safe, must have at least one exercise
+    @Delete
+    fun deleteExercise(exercise: Exercise)
+
+    @Transaction
+    fun deleteExerciseSafely(exercise: Exercise) {
+        val exerciseCount = getExerciseCount()
+        if (exerciseCount > 1) {
+            deleteExercise(exercise)
+        }
+    }
+
+    // SCHEDULE
+    @Delete
+    fun deleteScheduleDate(scheduleDate: ScheduleDate)
+
+    // Not safe, must have a non-empty schedule
+    @Query("DELETE FROM schedule_dates")
+    fun deleteAllScheduleDates()
 }
 
 
--- app/src/main/java/com/example/project2_adomingo/database/StronkLiftsTables.kt
+++ app/src/main/java/com/example/project2_adomingo/database/StronkLiftsTables.kt
@@ -4,40 +4,87 @@ import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.ForeignKey
-import androidx.room.Junction
 import androidx.room.Relation
 import java.time.DayOfWeek
 import java.time.LocalDate
 import kotlin.time.Duration
 
-@Entity(tableName = "workouts")
+@Entity(tableName = "users")
+data class User(
+    @PrimaryKey(autoGenerate = true) val userId: Long = 0,
+    var userName: String,
+    var description: String
+)
+
+// 1:N, a user has many workouts (in a workout plan)
+@Entity(tableName = "workouts",
+    foreignKeys = [
+        ForeignKey(
+            entity = User::class,
+            parentColumns = ["userId"],
+            childColumns = ["userId"],
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    primaryKeys = ["workoutId", "userId"]
+)
 data class Workout(
     @PrimaryKey(autoGenerate = true) val workoutId: Long = 0,
-    //val userId: Long,
+    val userId: Long,
     var workoutName: String,
     var description: String
 )
 
-// 1:N Workout-Date junction table: Workouts have many dates
-@Entity(tableName = "dates",
-    foreignKeys = [ForeignKey(
-        entity = Workout::class,
-        parentColumns = ["workoutId"],
-        childColumns = ["workoutId"],
-        onUpdate = ForeignKey.CASCADE,
-        onDelete = ForeignKey.CASCADE
-    )]
+// 1:N, a user has many workout dates 
+// (in either their workout schedule or workout history)
+@Entity(tableName = "schedule_dates",
+    foreignKeys = [
+        ForeignKey(
+            entity = User::class,
+            parentColumns = ["userId"],
+            childColumns = ["userId"],
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    primaryKeys = ["dateId", "userId", "weekday"]
 )
-data class Date(
+data class ScheduleDate(
     @PrimaryKey(autoGenerate = true) val dateId: Long = 0,
-    val workoutId: Long,
-    val date: DayOfWeek
+    val userId: Long,
+    val weekday: DayOfWeek
 )
 
-// Exercises independent of Workouts
-@Entity(tableName = "exercises")
+// Users workout schedule (e.g. Monday, Wednesday, Friday)
+// Determines order of workouts
+// Embedded for efficient querying
+//data class WorkoutSchedule(
+//    @Embedded val workout: User, // Embedded for efficient querying
+//    @Relation(
+//        parentColumn = "userId",
+//        entityColumn = "userId",
+//    )
+//    val exercises: List<ScheduleDate>, // 1:N, each user as many workout dates
+//)
+
+// Available exercises (independent of workouts)
+// 1:N, a user has many exercises to choose from
+@Entity(tableName = "exercises",
+    foreignKeys = [
+        ForeignKey(
+            entity = User::class,
+            parentColumns = ["userId"],
+            childColumns = ["userId"],
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    primaryKeys = ["exerciseId", "userId"]
+)
 data class Exercise(
     @PrimaryKey(autoGenerate = true) val exerciseId: Long = 0,
+    val userId: Long,
     val exerciseName: String,
     val equipment: Equipment,
     val muscleGroup: MuscleGroup
@@ -47,25 +94,34 @@ data class Exercise(
 @Entity(tableName = "workout_exercises",
     foreignKeys = [
         ForeignKey(
+            entity = User::class,
+            parentColumns = ["userId"],
+            childColumns = ["userId"],
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
             entity = Workout::class,
             parentColumns = ["workoutId"],
             childColumns = ["workoutId"],
+            onUpdate = ForeignKey.CASCADE,
             onDelete = ForeignKey.CASCADE
         ),
         ForeignKey(
             entity = Exercise::class,
             parentColumns = ["exerciseId"],
             childColumns = ["exerciseId"],
+            onUpdate = ForeignKey.CASCADE,
             onDelete = ForeignKey.CASCADE
         )
     ],
-    primaryKeys = ["workoutId", "exerciseId"]
+    primaryKeys = ["userId", "workoutId", "exerciseId"]
 )
 data class WorkoutExercise(
     @PrimaryKey(autoGenerate = true) val workoutExerciseId: Long = 0,
     val workoutId: Long,
     //val exerciseId: Long,
-    @Embedded
+    @Embedded // or could user exerciseId, but harder to query later
     val exercise: Exercise,
     val sets: Int,
     val reps: Int,
@@ -82,61 +138,69 @@ data class WorkoutPlan(
         entityColumn = "workoutId",
     )
     val exercises: List<WorkoutExercise>, // 1:N, each workout has many exercises
-    @Relation(
-        parentColumn = "workoutId",
-        entityColumn = "workoutId" // 1:N, each workout has many dates
-    )
-    val dates: List<Date>
 )
 
-//
-@Entity(tableName = "workout_history",
+// 1:N, a user has many workout dates (instances) in their history
+@Entity(tableName = "workout_history_dates",
     foreignKeys = [
         ForeignKey(
-            entity = Workout::class,
-            parentColumns = ["workoutId"],
-            childColumns = ["workoutId"],
+            entity = User::class,
+            parentColumns = ["userId"],
+            childColumns = ["userId"],
+            onUpdate = ForeignKey.CASCADE,
             onDelete = ForeignKey.CASCADE
         )
     ],
-    primaryKeys = ["workoutId"]
+    primaryKeys = ["workoutHistoryDateId, userId"]
 )
-data class WorkoutHistory(
-    @PrimaryKey(autoGenerate = true) val workoutHistoryId: Long = 0,
-    val workoutId: Long,
+data class WorkoutHistoryDate(
+    @PrimaryKey(autoGenerate = true) val workoutHistoryDateId: Long = 0,
+    val userId: Long,
     val date: LocalDate
 )
 
-//
+// 1:N, a user has many exercises in their workout sessions in history
 @Entity(tableName = "exercise_history",
     foreignKeys = [
         ForeignKey(
-            entity = WorkoutHistory::class,
-            parentColumns = ["workoutHistoryId"],
-            childColumns = ["workoutHistoryId"],
+            entity = User::class,
+            parentColumns = ["userId"],
+            childColumns = ["userId"],
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = WorkoutHistoryDate::class,
+            parentColumns = ["workoutHistoryDateId"],
+            childColumns = ["workoutHistoryDateId"],
+            onUpdate = ForeignKey.CASCADE,
             onDelete = ForeignKey.CASCADE
         ),
         ForeignKey(
             entity = Exercise::class,
             parentColumns = ["workoutExerciseId"],
             childColumns = ["workoutExerciseId"],
+            onUpdate = ForeignKey.CASCADE,
             onDelete = ForeignKey.CASCADE
         )
     ],
-    primaryKeys = ["workoutHistoryId", "workoutExerciseId"]
+    primaryKeys = ["exerciseHistoryId", "userId,", "workoutHistoryDateId", "workoutExerciseId"]
 )
 data class ExerciseHistory(
     @PrimaryKey(autoGenerate = true) val exerciseHistoryId: Long = 0,
-    val workoutHistoryId: Long,
-    @Embedded
+    val userId: Long,
+    val workoutHistoryDateId: Long,
+    @Embedded // easier join
     val workoutExercise: WorkoutExercise
 )
 
-data class WorkoutSession(
-    @Embedded val workout: WorkoutHistory,
+// Workout History with workout date and exercise history
+// Embedded for efficient querying
+data class WorkoutHistory(
+    @Embedded val workout: WorkoutHistoryDate,
     @Relation(
-        parentColumn = "workoutHistoryId",
-        entityColumn = "workoutHistoryId",
+        parentColumn = "workoutHistoryDateId",
+        entityColumn = "workoutHistoryDateId",
     )
     val exercises: List<ExerciseHistory>, // 1:N, each workout history had many exercise histories
 )
